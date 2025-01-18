b1660e18f2bc5e072e22a8518cf64debd0416165
Walter Santos <walterbass33@gmail.com>
2025-01-18 16:07:09 -0300

    beatles

---
+++ beatles.py
@@ -0,0 +1,19 @@
+beatles =[]
+
+#Etapa 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print(beatles)
+
+#Etapa 2
+for i in beatles:
+    beatles.append(input("Adicione Stu Sutcliffe: "))
+    beatles.append(input("Adicione Pete Best: "))
+    print (beatles)
+    del beatles [3]
+    del beatles [3]
+    break
+print(beatles)
+beatles.insert(3, "Ringo Star")
+print (beatles)
